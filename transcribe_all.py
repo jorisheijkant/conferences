957154bdadc565afff0f967136bfb5abbde2286d
jorisheijkant <jorisheijkant@persgroep.net>
2024-12-09 17:36:51 +0100

    Fixed output to text folder

--- transcribe_all.py
+++ transcribe_all.py
@@ -5,6 +5,7 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context # Not a fan, but needed to get whisper to work on my laptop
 
 source_folder = "audio_source"
+text_folder = "texts"
 
 for (folders, labels, files) in os.walk(source_folder):
     for file in files:
@@ -13,10 +14,14 @@ for (folders, labels, files) in os.walk(source_folder):
             file_id = file.split(".")[0]
             print(f"Now transcribing {file}")
 
-            model = whisper.load_model("base")
+            if os.path.exists(f"{text_folder}/{file_id}.txt"):
+                print(f"File {file} already transcribed, skipping...")
+                continue
+
+            model = whisper.load_model("large") # Change this to a better model if your setup allows
             result = model.transcribe(full_path)
             print(result["text"])
 
-            with open(f"{source_folder}/{file_id}.txt", "w") as text_output:
+            with open(f"{text_folder}/{file_id}.txt", "w") as text_output:
                 text_output.write(result["text"])
 
